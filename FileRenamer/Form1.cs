541bdbdbb9232a8ac8950f1b1b98903094fa9f44
AGhostik <a-ghostik@yandex.ru>
2017-03-31 14:46:55 +0500

    Выбор папки, защита от дурака, выбор файлов в список и сортировка по размеру

--- FileRenamer/Form1.cs
+++ FileRenamer/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace FileRenamer
 {
@@ -17,14 +18,36 @@ namespace FileRenamer
             InitializeComponent();
         }
 
+        //var
+        IOrderedEnumerable<string> files;
+        //var
+
         private void button_rename_Click(object sender, EventArgs e)
         {
-
+            if (Directory.Exists(textBox_folderPath.Text))
+            {
+                foreach (string s in files)
+                    MessageBox.Show(s);
+            }
         }
 
         private void button_browse_Click(object sender, EventArgs e)
         {
+            selectFolder();
+        }
 
+        private void sortFiles()
+        {           
+            files = Directory.GetFiles(textBox_folderPath.Text).OrderBy(f => new FileInfo(f).Length); //магическая строка, якобы сортирующая файлы по размеру
+        }
+
+        private bool getFileList()
+        {
+            bool filesExist = 
+                Directory.GetFiles(textBox_folderPath.Text).Length > 0 
+                ? true : false;
+
+            return filesExist;
         }
 
         private void selectFolder()
@@ -34,6 +57,15 @@ namespace FileRenamer
                 if (folderDialog.ShowDialog() == DialogResult.OK)
                 {
                     textBox_folderPath.Text = folderDialog.SelectedPath;
+
+                    if (getFileList())
+                    {
+                        sortFiles();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Folder does not contain files");
+                    }
                 }
             }
         }
